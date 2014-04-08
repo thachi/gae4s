2174ca5a0a77093821317ca751fe3e6d53a1bd57
Takashi Hachinohe <takashi.hachinohe@gmail.com>
2014-04-08 19:30:46 +0900

    DataSeqEntityを追加しました。

--- src/main/scala/com/xhachi/gae4s/datastore/DataEntity.scala
+++ src/main/scala/com/xhachi/gae4s/datastore/DataEntity.scala
@@ -8,9 +8,25 @@ abstract class DataEntity[E <: DataEntity[E, D], D <: AnyRef : ClassTag] extends
 }
 
 abstract class DataEntityMeta[E <: DataEntity[E, D] : ClassTag, D <: AnyRef : Manifest] extends EntityMeta[E] {
-  //  type Entity <: DataEntity[E, D]
 
   val data: JsonProperty[D] = new JsonProperty[D]("data")
+
+  addApplyToLLEntity {
+    (from: Entity, to: LLEntity) => data.setToStore(from.data)(to)
+  }
+
+  addApplyFromLLEntity {
+    (from: LLEntity, to: Entity) => to.data = data.getFromStore(from)
+  }
+}
+
+abstract class DataSeqEntity[E <: DataSeqEntity[E, D], D <: AnyRef : ClassTag] extends Entity[E] {
+  var data: Seq[D] = Nil
+}
+
+abstract class DataSeqEntityMeta[E <: DataSeqEntity[E, D] : ClassTag, D <: AnyRef : Manifest] extends EntityMeta[E] {
+
+  val data: JsonProperty[Seq[D]] = new JsonProperty[Seq[D]]("data")
 
   addApplyToLLEntity {
     (from: Entity, to: LLEntity) => data.setToStore(from.data)(to)
