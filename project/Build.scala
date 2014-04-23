00d7e84837a1b29ca75451a9c421d148d5ff5888
Takashi Hachinohe <takashi.hachinohe@gmail.com>
2014-04-23 10:13:57 +0900

    2.10.3と2.11.0のクロスビルドを設定しました。 公開リポジトリの設定を変更しました。

--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,17 @@ object Gae4sBuild extends Build {
     Seq(
       sourceGenerators in Compile <+= buildInfo,
       buildInfoKeys := Seq[BuildInfoKey](name, version, scalaVersion),
-      scalaVersion := "2.10.3",
+      scalaVersion := "2.11.0",
+      crossScalaVersions := Seq("2.10.3", "2.11.0"),
       organization := "com.xhachi",
       version := "0.5-SNAPSHOT",
-      publishTo := Some(Resolver.file("gae4s",file("/Users/takashi/Documents/xhachi/maven/maven"))(Patterns(true, Resolver.mavenStyleBasePattern))),
+      publishTo <<= version { (v: String) =>
+        val base = "/Users/takashi/Documents/xhachi/repository"
+        if (v.trim.endsWith("SNAPSHOT"))
+          Some(Resolver.file("snapshot", file(base + "/maven2-snapshot")))
+        else
+          Some(Resolver.file("release" , file(base + "/maven2")))
+      },
       parallelExecution in Test := false
     )
 
@@ -30,13 +37,13 @@ object Gae4sBuild extends Build {
   val defaultDependency = {
     val appengineVersion = "1.9.1"
     Seq(
-      "org.json4s" %% "json4s-native" % "3.2.8",
+      "org.json4s" %% "json4s-native" % "3.2.9",
       "com.google.appengine" % "appengine-api-1.0-sdk" % appengineVersion,
       "com.google.appengine.tools" % "appengine-gcs-client" % "0.3.9",
       "com.google.appengine" % "appengine-api-stubs" % appengineVersion % "test",
       "com.google.appengine" % "appengine-testing" % appengineVersion % "test",
-      "org.slf4j" % "slf4j-jdk14" % "1.7.5" % "test",
-      "org.scalatest" %% "scalatest" % "2.0" % "test"
+      "org.slf4j" % "slf4j-jdk14" % "1.7.5",
+      "org.scalatest" %% "scalatest" % "2.1.3" % "test"
     )
   }
 }
