b515a82705aafd536796f0b440022d7a6dfb5863
Takashi Hachinohe <takashi.hachinohe@gmail.com>
2014-09-28 17:26:32 +0900

    AppInfoで取得できる値をOptionで取得するようにしました。

--- src/main/scala/com/xhachi/gae4s/common/AppInfo.scala
+++ src/main/scala/com/xhachi/gae4s/common/AppInfo.scala
@@ -7,8 +7,19 @@ import com.google.appengine.api.utils.SystemProperty
  */
 object AppInfo {
 
+  @deprecated
   def version = SystemProperty.applicationVersion.get()
 
+  def versionOption: Option[String] = SystemProperty.applicationVersion.get() match {
+    case v: String => Some(v)
+    case _ =>  None
+  }
+
+  @deprecated
   def id = SystemProperty.applicationId.get()
 
+  def idOption: Option[String] = SystemProperty.applicationId.get() match {
+    case i: String => Some(i)
+    case _ =>  None
+  }
 }
