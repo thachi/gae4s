b92d3bcd403a954cd4ca6f5ecd87f3f3ee3c2576
Takashi Hachinohe <thachi@xhachiapps.com>
2014-12-12 15:09:29 +0900

    Loggerの内部を変更しました。

--- src/main/scala/com/xhachi/gae4s/common/Logger.scala
+++ src/main/scala/com/xhachi/gae4s/common/Logger.scala
@@ -1,24 +1,28 @@
 package com.xhachi.gae4s.common
 
-import org.slf4j.LoggerFactory
+import java.util.logging.{Level, Logger}
 
 
 trait Logger {
 
-  lazy val logger = LoggerFactory.getLogger(this.getClass)
+  lazy val logger = Logger.getLogger(this.getClass.toString)
 
-  def trace(message: => String): Unit = if (logger.isTraceEnabled) logger.trace(message)
+  def trace(message: => String): Unit = if (logger.isLoggable(Level.FINER)) logger.finer(message)
 
-  def debug(message: => String): Unit = if (logger.isDebugEnabled) logger.debug(message)
+  def debug(message: => String): Unit = if (logger.isLoggable(Level.FINE)) logger.fine(message)
 
-  def info(message: => String): Unit = if (logger.isInfoEnabled) logger.info(message)
+  def info(message: => String): Unit = if (logger.isLoggable(Level.INFO)) logger.info(message)
 
-  def warn(message: => String): Unit = if (logger.isWarnEnabled) logger.warn(message)
+  def warn(message: => String): Unit = if (logger.isLoggable(Level.WARNING)) logger.warning(message)
 
-  def error(message: => String): Unit = if (logger.isErrorEnabled) logger.error(message)
+  def error(message: => String): Unit = if (logger.isLoggable(Level.SEVERE)) logger.severe(message)
 
-  def warn(message: => String, e: Throwable): Unit = if (logger.isWarnEnabled) logger.warn(message, e)
+  def warn(message: => String, e: Throwable): Unit = if (logger.isLoggable(Level.WARNING)) {
+    logger.warning(message + "\n" + e.getStackTrace.map(_.toString).mkString("\n"))
+  }
 
-  def error(message: => String, e: Throwable): Unit = if (logger.isErrorEnabled) logger.error(message, e)
+  def error(message: => String, e: Throwable): Unit = if (logger.isLoggable(Level.SEVERE)) {
+    logger.severe(message + "\n" + e.getStackTrace.map(_.toString).mkString("\n"))
+  }
 
 }
