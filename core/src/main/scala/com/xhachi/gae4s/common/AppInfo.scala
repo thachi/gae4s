146009ba60117a1bba70706c4592bb4642c2c871
Takashi Hachinohe <thachi@xhachiapps.com>
2015-02-20 18:50:47 +0900

    AppInfoで環境情報を取得できるようにしました。

--- core/src/main/scala/com/xhachi/gae4s/common/AppInfo.scala
+++ core/src/main/scala/com/xhachi/gae4s/common/AppInfo.scala
@@ -7,13 +7,34 @@ import com.google.appengine.api.utils.SystemProperty
  */
 object AppInfo {
 
+  val Runtime = "com.google.appengine.runtime"
+
+  val EnvironmentKey = Runtime + ".environment"
+
+  def version: String = versionOption.getOrElse(throw new IllegalStateException("version is not defined."))
+
   def versionOption: Option[String] = SystemProperty.applicationVersion.get() match {
     case v: String => Some(v)
-    case _ =>  None
+    case _ => None
   }
+
+  def id: String = idOption.getOrElse(throw new IllegalStateException("id is not defined."))
 
   def idOption: Option[String] = SystemProperty.applicationId.get() match {
     case i: String => Some(i)
-    case _ =>  None
+    case _ => None
   }
+
+  def environment = System.getProperty(EnvironmentKey) match {
+    case null => None
+    case e => Some(e)
+  }
+
+  def isServer: Boolean = environment.isDefined
+
+  def isDevelopment: Boolean = environment.contains("Development")
+
+  def isProduction: Boolean = environment.contains("Production")
+
+
 }
