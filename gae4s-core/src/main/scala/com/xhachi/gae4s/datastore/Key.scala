a782114d9df92b0e5fee5d58c470a026697ff961
Takashi Hachinohe <takashi.hachinohe@gmail.com>
2016-09-05 21:43:26 +0900

    KeyにParentを指定できるメソッドを追加しました。

--- gae4s-core/src/main/scala/com/xhachi/gae4s/datastore/Key.scala
+++ gae4s-core/src/main/scala/com/xhachi/gae4s/datastore/Key.scala
@@ -38,14 +38,24 @@ case class Key(key: LLKey) extends Ordered[Key] {
   def toWebSafeString = KeyFactory.keyToString(key)
 
   override def compare(that: Key): Int = this.key.compareTo(that.key)
+
+  def withParent(parent: Key): Key = idOption match {
+    case Some(i) => Key(key.getKind, i, Some(parent))
+    case None => Key(key.getKind, name, Some(parent))
+  }
 }
 
 object Key {
 
   def fromWebSafeString(string: String): Key = Key(KeyFactory.stringToKey(string))
 
-  def apply(kind: String, id: Long): Key = Key(KeyFactory.createKey(kind, id))
+  def apply(kind: String, id: Long, parent: Option[Key]): Key = parent match {
+    case Some(p) => Key(KeyFactory.createKey(p.key, kind, id))
+    case None => Key(KeyFactory.createKey(kind, id))
+  }
 
-  def apply(kind: String, name: String): Key = Key(KeyFactory.createKey(kind, name))
-
+  def apply(kind: String, name: String, parent: Option[Key]): Key = parent match {
+    case Some(p) => Key(KeyFactory.createKey(p.key, kind, name))
+    case None => Key(KeyFactory.createKey(kind, name))
+  }
 }
